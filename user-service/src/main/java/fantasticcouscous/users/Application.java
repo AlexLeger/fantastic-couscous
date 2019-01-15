6ff9bbf052952d1ba9d49cb56a0b3bfd8562b29e
AlexLeger <alex.m.leger@gmail.com>
2019-01-15 10:28:17 +0100

    Fixed HttpRequestTest (wrong Gruber brother)

--- user-service/src/main/java/fantasticcouscous/users/Application.java
+++ user-service/src/main/java/fantasticcouscous/users/Application.java
@@ -1,14 +1,11 @@
 package fantasticcouscous.users;
 
-import fantasticcouscous.users.model.UserData;
-import fantasticcouscous.users.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
@@ -19,45 +16,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner demo(UserRepository userRepository) {
-        return (args) -> {
-            // save a couple of users
-            userRepository.save(new UserData("jmcclane", "John"));
-            userRepository.save(new UserData("hgruber", "Hans"));
+    /* Removed CommandLineRunner because WebLayerTest failed with
+    Caused by: java.lang.IllegalStateException: Failed to execute CommandLineRunner */
 
-
-            // fetch all users
-            log.info("Users found with findAll():");
-            log.info("-------------------------------");
-            for (UserData user : userRepository.findAll()) {
-                log.info(user.toString());
-            }
-            log.info("");
-
-            // fetch an individual user by login
-            log.info("User found with login jmclane: ");
-            UserData loginuser = userRepository.findOneByLogin("jmcclane");
-            log.info(loginuser.toString());
-            // fetch an individual user by ID
-            /*userRepository.findById(1L)
-                    .ifPresent(customer -> {
-                        log.info("Customer found with findById(1L):");
-                        log.info("--------------------------------");
-                        log.info(customer.toString());
-                        log.info("");
-                    });*/
-
-            // fetch users by first name
-            log.info("Customer found with findByFirstName('John'):");
-            log.info("--------------------------------------------");
-            userRepository.findByFirstName("John").forEach(user -> {
-                log.info(user.toString());
-            });
-            // for (Customer bauer : repository.findByLastName("Bauer")) {
-            // 	log.info(bauer.toString());
-            // }
-            log.info("");
-        };
-    }
 }
--- user-service/src/test/java/fantasticcouscous/users/HttpRequestTest.java
+++ user-service/src/test/java/fantasticcouscous/users/HttpRequestTest.java
@@ -1,8 +1,14 @@
 package fantasticcouscous.users;
 
+import fantasticcouscous.users.model.UserData;
+import fantasticcouscous.users.repository.UserRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -18,6 +24,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     @LocalServerPort
     private int port;
     /* Note the use of webEnvironment=RANDOM_PORT to start the server with a random port (useful to avoid conflicts in test environments),
@@ -26,7 +34,28 @@ public class HttpRequestTest {
     private String endpoint = "user";
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private TestRestTemplate restTemplate; //TestRestTemplate is provided by SpringBoot, we just have to autowire it.
+
+    @Before
+    public void init_userRepository(){
+        //Adds a few users to the repository for userServiceShouldReturnUserData test
+        userRepository.save(new UserData("jmcclane", "John"));
+        userRepository.save(new UserData("sgruber", "Simon"));
+    }
+
+    @Test
+    public void userRepositoryShouldNotBeNull(){
+        assertThat(userRepository).isNotNull();
+    }
+
+    @Test
+    public void setUserRepositoryShouldBeInitWithUsers(){
+        log.info("userRepository contains "+userRepository.count()+ " users");
+        assertThat(userRepository.count()).isEqualTo(2);
+    }
 
     @Test
     public void userServiceShouldReturnServiceInfo() throws Exception {
@@ -40,8 +69,11 @@ public class HttpRequestTest {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/" + endpoint + "/" + login,
                 String.class)).contains("{\"login\":\"jmcclane\",\"firstName\":\"John\"}");
 
-        login = "hgruber";
+        login = "sgruber";
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/" + endpoint + "/" + login,
-                String.class)).contains("{\"login\":\"hgruber\",\"firstName\":\"Hans\"}");
+                String.class)).contains("{\"login\":\"sgruber\",\"firstName\":\"Simon\"}");
     }
+
+    @After
+    public void clean() { userRepository.deleteAll(); }
 }
