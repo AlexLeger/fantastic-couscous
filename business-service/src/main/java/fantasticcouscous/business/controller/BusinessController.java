19045d33cd4bea52e23373d31966f9f201ff36b7
AlexLeger <alex.m.leger@gmail.com>
2019-01-15 17:29:27 +0100

    Added business service

---
+++ business-service/src/main/java/fantasticcouscous/business/controller/BusinessController.java
@@ -0,0 +1,13 @@
+package fantasticcouscous.business.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BusinessController {
+
+    @RequestMapping(value = "/service_info")
+    public String getServiceInfo() {
+        return "This is the business service."; //TODO Replace by application property (or switch to Actuator)
+    }
+}
