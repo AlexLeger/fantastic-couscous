23c151d3d0eac768eb48013be5381896a1337c02
AlexLeger <alex.m.leger@gmail.com>
2019-01-22 14:20:11 +0100

    Shifting cached method to the controller class (where it is called) breaks cache as expected

--- business-service/src/main/java/fantasticcouscous/business/controller/BusinessController.java
+++ business-service/src/main/java/fantasticcouscous/business/controller/BusinessController.java
@@ -1,12 +1,18 @@
 package fantasticcouscous.business.controller;
 
-import fantasticcouscous.business.services.BusinessService;
+import fantasticcouscous.business.UserServiceProxy;
+import fantasticcouscous.business.foreign.UserData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@EnableCaching
 @RestController
 public class BusinessController {
 
@@ -14,7 +20,7 @@ public class BusinessController {
     private String applicationName;
 
     @Autowired
-    private BusinessService businessService;
+    UserServiceProxy userServiceProxy;
 
     @GetMapping(value = "/service_info",
             produces = { "application/json" })
@@ -25,7 +31,15 @@ public class BusinessController {
     @GetMapping(value = "/business/{login}",
             produces = { "application/json" })
     public String getBusinessInfo(@PathVariable("login") String login) {
-        return businessService.performBusinessOperation(login);
+        return performBusinessOperation(login);
+    }
+
+    @Cacheable(value = "user", key = "{#login}")
+    public String performBusinessOperation(String login){
+        log.info("Retrieving user data from userServiceProxy for login : {}",login);
+        UserData userData = userServiceProxy.getUserInfo(login);
+        log.info("Retrieved user data  : {}",userData);
+        return "Business operation is performed. Retrieved data for user : " + userData.toString();
     }
 
 }
