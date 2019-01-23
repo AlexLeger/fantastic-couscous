6b99776adb88400490ffd858eecc9fe2322e6d89
AlexLeger <alex.m.leger@gmail.com>
2019-01-23 10:16:16 +0100

    Brought back Business Service (so cache is working again) and add registry to sonar

--- business-service/src/main/java/fantasticcouscous/business/controller/BusinessController.java
+++ business-service/src/main/java/fantasticcouscous/business/controller/BusinessController.java
@@ -1,18 +1,12 @@
 package fantasticcouscous.business.controller;
 
-import fantasticcouscous.business.UserServiceProxy;
-import fantasticcouscous.business.foreign.UserData;
-import lombok.extern.slf4j.Slf4j;
+import fantasticcouscous.business.services.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@EnableCaching
 @RestController
 public class BusinessController {
 
@@ -20,7 +14,7 @@ public class BusinessController {
     private String applicationName;
 
     @Autowired
-    UserServiceProxy userServiceProxy;
+    private BusinessService businessService;
 
     @GetMapping(value = "/service_info",
             produces = { "application/json" })
@@ -31,15 +25,7 @@ public class BusinessController {
     @GetMapping(value = "/business/{login}",
             produces = { "application/json" })
     public String getBusinessInfo(@PathVariable("login") String login) {
-        return performBusinessOperation(login);
-    }
-
-    @Cacheable(value = "user", key = "{#login}")
-    public String performBusinessOperation(String login){
-        log.info("Retrieving user data from userServiceProxy for login : {}",login);
-        UserData userData = userServiceProxy.getUserInfo(login);
-        log.info("Retrieved user data  : {}",userData);
-        return "Business operation is performed. Retrieved data for user : " + userData.toString();
+        return businessService.performBusinessOperation(login);
     }
 
 }
---
+++ business-service/src/main/java/fantasticcouscous/business/services/BusinessService.java
@@ -0,0 +1,30 @@
+package fantasticcouscous.business.services;
+
+import fantasticcouscous.business.UserServiceProxy;
+import fantasticcouscous.business.foreign.UserData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableCaching
+@Slf4j
+public class BusinessService {
+
+    @Autowired
+    UserServiceProxy userServiceProxy;
+
+    /* In the future, this class will provide the services depending on the calling user's permissions.
+    For now it will just call the user service to get the user's information. */
+
+    @Cacheable(value = "user", key = "{#login}")
+    public String performBusinessOperation(String login){
+        log.info("Retrieving user data from userServiceProxy for login : {}",login);
+        UserData userData = userServiceProxy.getUserInfo(login);
+        log.info("Retrieved user data  : {}",userData);
+        return "Business operation is performed. Retrieved data for user : " + userData.toString();
+    }
+
+}
