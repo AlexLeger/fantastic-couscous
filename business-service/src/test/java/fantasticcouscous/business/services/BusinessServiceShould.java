801a4e5b26b67154d1b952eddb43a332b5966b9f
AlexLeger <alex.m.leger@gmail.com>
2019-01-24 15:33:50 +0100

    Added test to see what is shared or not between tests and what needs to be reset

--- business-service/src/test/java/fantasticcouscous/business/services/BusinessServiceShould.java
+++ business-service/src/test/java/fantasticcouscous/business/services/BusinessServiceShould.java
@@ -1,21 +1,16 @@
 package fantasticcouscous.business.services;
 
 import fantasticcouscous.business.application.TestApplication;
-import feign.FeignException;
 import feign.Request;
-import feign.Response;
-import feign.codec.DecodeException;
-import feign.codec.Decoder;
 import feign.mock.HttpMethod;
 import feign.mock.MockClient;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,9 +34,7 @@ public class BusinessServiceShould {
     public void hitMock(){
     String login ="jmcclane";
         String result = businessService.performBusinessOperation(login);
-        /*assertThat(result,notNullVamockClientlue());
-        assertThat(result.getLogin(),equals(login));
-       assertThat(result.getFirstName(),equals("John"));*/
+        assertThat(result).isNotNull();
         assertThat(result).contains(login);
     }
 
@@ -60,6 +53,22 @@ public class BusinessServiceShould {
         List<Request> results = mockClient.verifyTimes(HttpMethod.GET, "/user/"+login, 1); //TODO Understand why the expected number of times has to be checked twice (once in verify, once in results size)
         log.info("Test cache : {}",results.toString());
         assertThat(results).hasSize(1);
-        mockClient.verifyStatus();
+        //mockClient.verifyStatus(); //TODO find out what that's for
     }
+
+    @Test
+    public void firstCallDoesntHitCache(){
+        String login ="jmcclane";
+        businessService.performBusinessOperation(login);
+        List<Request> results = mockClient.verifyTimes(HttpMethod.GET, "/user/"+login, 1);
+        assertThat(results).hasSize(1);
+
+        String login2 ="hgruber";
+        businessService.performBusinessOperation(login2);
+        results = mockClient.verifyTimes(HttpMethod.GET, "/user/"+login2, 1);
+        assertThat(results).hasSize(1);
+
+    }
+
 }
+
